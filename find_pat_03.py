7ac9773d448a2ca36e9f36caa282fb5bdcf8c3f4
sadrawi <dr.sadrawi@gmail.com>
2025-06-24 17:05:40 +0700

    find_pat_03.py

--- find_pat_03.py
+++ find_pat_03.py
@@ -49,39 +49,5 @@ if st.button("Find the Patterns"):
     st.dataframe(all_pats, use_container_width=True)
 
     print(all_pats)
-    # for j in range(0,len(all_pats)):
-
-    #     all_ind.append(tuple(all_pats[j]['index']))
-    # # st.write(all_pats[0]['index'])
-    # st.write(all_ind)
-
-    # for j in range(0,len(all_pats)):
-    #     print(all_pats['index'])
-    # st.write(len(all_pats))
-    # # if match1:
-    # for i in range(0,len(pos_all1)-1):
-    #     p_pos.append((pos_all1[i],pos_all1[i+1]))
-    # # st.write(f'Paired: {str(p_pos)}')
-    # for i in range(0,len(p_pos)):
-    #     l_pair.append(p_pos[i][1]-p_pos[i][0])
-    #     s_pair.append(fasta[p_pos[i][0]:p_pos[i][1]])
-    # # st.write(f'Segment: {s_pair}')
-    # # st.write(f'Segment Length: {l_pair}')
-
-    # data = {
-    # 'Sequence': s_pair,
-    # 'Length': tuple(l_pair)
-    # }
-
-    # # Create DataFrame
-    # df = pd.DataFrame(data)
-
-    # # Reorder columns and sort
-    # s_df = df[['Sequence', 'Length']].sort_values(
-    #     by='Length', ascending=False).reset_index(drop=True)
-
-    # # Display the DataFrame
-    # # st.dataframe(s_df, use_container_width=True)
-    # st.write(s_df)
-
+    
 
